6a3546be74b57d4cf24a623676ea637a4a40cf22
Adam-Lowther <139771204+Adam-Lowther@users.noreply.github.com>
2023-08-14 21:09:46 +0100

    Add files via upload

---
+++ Day 13- K-Means clustering.R
@@ -0,0 +1,30 @@
+#K-Means Clustering
+
+#Importing the Mall dataset
+dataset <- read.csv('mall.csv')
+X <- dataset[4:5]
+
+#Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss <- vector()
+for (i in 1:10) wcss[i] <- sum(kmeans(X , i)$withinss)
+plot(1:10, wcss, type = "b", main = paste('Clusters of clients'), xlab = 'Number of clusters', ylab = "wcss")
+
+
+#Applying k-means to the mall dataset
+set.seed(29)
+kmeans <- kmeans(X, 5, iter.max = 300, nstart = 10)
+
+#Visualising the clusters
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Cluster of Clients'),
+         xlab = "Annual Income",
+         ylab = "Spending Score")
