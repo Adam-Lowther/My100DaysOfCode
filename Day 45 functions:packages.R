3909703cac1c8651211898f033869c00b39d7006
Adam-Lowther <139771204+Adam-Lowther@users.noreply.github.com>
2023-09-26 11:43:56 +0100

    Add files via upload

---
+++ Day 45 functions:packages.R
@@ -0,0 +1,40 @@
+#Day 45
+
+x <- c("a","b","c")
+
+rnorm(5,10,8) #so 68.2% of the variables will fall between 2 and 18
+
+c()
+A <- seq(from = 10, to=20, length.out=100) #here instead of using by (for the step) we using length.out for n of numbers and calculates step accordingly. along.with (to be same length as specified vector)
+rep(x, each= 10)
+
+print()
+
+is.numeric() 
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+A
+B <- sqrt(A)
+B
+paste()
+
+#?
+#####################
+
+install.packages("ggplot2")
+library(ggplot2)
+
+?qplot()
+?ggplot()
+?diamonds
+
+qplot(data = diamonds, carat, price,
+      colour=clarity, facets=.~clarity)
+
+####################
+
+
